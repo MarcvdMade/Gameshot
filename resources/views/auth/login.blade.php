f4bf86c825153a758b27179f6c16d29c16dccebe
MarcvdMade <55003488+MarcvdMade@users.noreply.github.com>
2020-09-22 15:21:09 +0200

    pushed unversioned files

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.pagelayout')
+
+@section('content')
+<div class="content">
+    <div class="d-flex justify-content-center padding-bottom">
+        <div class="d-inline-flex flex-column p-2 form-box">
+            <div class="">
+                <div class="text-center text-uppercase"><h2>{{ __('Login') }}</h2></div>
+
+                <div class="">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="m-auto">
+                                <button type="submit" class="submit-input">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="m-auto">
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container no-gutters">
     <div class="">
-        <h2>Welcome {{Auth::user()->name}}</h2>
+        <h2>Welcome, {{Auth::user()->name}}</h2>
         @if (session('status'))
             <div class="alert alert-success" role="alert">
                 {{ session('status') }}
